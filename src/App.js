e8d3f45add6eb2c4074ff97920f9a0302dca6a5e
srimalonline <dilankasrimal23@gmail.com>
2022-12-12 10:50:17 +0530

    starting movie web development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
-const Person = (props) => {
-  return(
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: Fernando</h2>
-    <h2>Age: 30</h2>
-    </>
-  )
-}
-const App = () =>{
-  const [counter, setCounter] = useState(0);
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
-    </div>
-  );
-}
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=c801b5d3";
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s={title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies("Spiderman");
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Movie Lanka</h1>
+            <div className="search">
+                <input
+                    placeholder="Search from movies"
+                    value="Superman"
+                    onChange={() => { }}
+                />
+                <img src={SearchIcon} alt="search" onClick={() => { }} />
+            </div>
+
+            {movies?.length > 0 
+                ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard moive={movie} />
+                    ))}
+                </div>
+                ) : (
+                <div className="empty">
+                    <h2>No Movies found</h2>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default App;
